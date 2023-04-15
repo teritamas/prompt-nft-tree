034c05c5ad1a8271a8e64a7d99277c1b615e424b
sey323 <sey323_nm@yahoo.co.jp>
2023-04-16 04:06:05 +0900

    LITのモーダルの前に移動させる。

--- packages/app/src/components/ModalCreateNft.svelte
+++ packages/app/src/components/ModalCreateNft.svelte
@@ -158,6 +158,7 @@
   id="extralarge-modal"
   tabindex="-1"
   class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+  style="z-index: 1"
 >
   <div class="relative w-full max-w-7xl max-h-full">
     <!-- Modal content -->
