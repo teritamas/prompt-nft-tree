b60c069bc3f06f891f5701f28d710806724bf0f9
sey323 <sey323_nm@yahoo.co.jp>
2023-04-15 20:38:07 +0900

    アカウント作成位置を修正

--- packages/app/src/components/FirstViewTitle.svelte
+++ packages/app/src/components/FirstViewTitle.svelte
@@ -1,3 +1,17 @@
+<script lang="ts">
+  // @ts-nocheck
+  import { downloadImage, getNft } from "../facades/database";
+  import { nftId, nftList, wagmiClient } from "../stores";
+  import { createAccount } from "../facades/database";
+      
+  let walltaddress = '';
+  wagmiClient.subscribe(async (value)=>{
+    walltaddress = value.data?.account;
+    createAccount(walltaddress)
+    .then(_=>console.log("アカウントチェック完了"));
+  });
+</script>
+
 <h1
   class="mt-8 text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"
 >
