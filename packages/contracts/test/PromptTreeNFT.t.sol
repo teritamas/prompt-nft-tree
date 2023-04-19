6a872882bb04da824132966cd185246b0c043e4b
sey323 <sey323_nm@yahoo.co.jp>
2023-04-19 22:48:37 +0900

    add unit test

--- packages/contracts/test/PromptTreeNFT.t.sol
+++ packages/contracts/test/PromptTreeNFT.t.sol
@@ -5,8 +5,16 @@ import "forge-std/Test.sol";
 import {PromptTreeNFT} from "../src/PromptTreeNFT.sol";
 
 contract PromptTreeNFTTest is Test {
-    // function testFoo() public {
-    //     Foo foo = new Foo();
-    //     assertEq(foo.myString(), "Hello, world!");
-    // }
+    function testMintResult() public {
+        PromptTreeNFT promptTreeNFT = new PromptTreeNFT();
+        
+        address testAddress = 0x7FF84a54d3d7070391Dd9808696Fc547a910af91;
+        string memory testencryptedPrompts = "hogehgoe";
+        uint testSourceNftTokenId = 1;
+
+        promptTreeNFT.mintNft(testAddress,testencryptedPrompts,testSourceNftTokenId );
+        
+        assertEq(promptTreeNFT.getSourcePromptId(1), testSourceNftTokenId);
+        assertEq(promptTreeNFT.getEncryptedPrompt(1), testencryptedPrompts);
+    }
 }
